3c48ecdfa2b8799391aba63d1e7cda003b29c991
RomanKorntheuer <romankorntheuer@gmail.com>
2018-11-28 18:31:04 +0100

    Update InputText, FlatList

--- MyJournal/App.js
+++ MyJournal/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {items: [] };
+
+  _addItem(text){
+    this.setState({
+      items: [...this.state.items, {text, date: Date.now() }]
+    });
+  }
+
   render() {
+    let content= <Text>Keine Einträge im Tagebuch</Text>;
+    if (this.state.items.length > 0) {
+      content= (
+        <FlatList
+        style={styles.list}
+        data={this.state.items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+        keyExtractor={item => item.date}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      {content}
+        <TextInput 
+        style={styles.input} 
+        placeholder="Tagebucheintrag erstellen"
+        returnKeyType="done"
+        onSubmitEditing={event => this._addItem(event.nativeEvent.text)}
+        ></TextInput>
       </View>
     );
   }
@@ -14,8 +39,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+
+  list: {
+    marginTop: 24
+  },
+
+  input: {
+    height: 40
+    
+  }
 });
